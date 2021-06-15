7022bfc036e010895079ce586b150fa1bc6825a1
Tyler Thomas <tygthomas0@gmail.com>
2021-06-15 17:02:55 -0600

    added comments

--- level-1/problem-solving-practice/main.js
+++ level-1/problem-solving-practice/main.js
@@ -1,3 +1,4 @@
+//uses spread to take an array of numbers of any size and find the largest number amongst them
 function largest(...nums) {
     let largest = nums[0];
     for (let i = 1; i < nums.length; i++) {
@@ -11,7 +12,7 @@ function largest(...nums) {
 
 largest(3, 4, 5, 2, 1);
 
-
+//checks to see if a number is evenly divisible by another number
 function isDivisible(num1, num2) {
     if (num1 % num2 == 0) {
         console.log(num1 + " is divisible by " + num2 + ".");
@@ -25,7 +26,7 @@ isDivisible(4, 2);
 isDivisible(9, 3);
 isDivisible(15, 4);
 
-
+//checks to see if a string has a particular character within the string, using an array of strings with spread
 function hasChar(char, ...strings) {
     var pos = [];
     for (let i = 0; i < strings.length; i++) {
