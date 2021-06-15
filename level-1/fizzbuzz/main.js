c7507ba10dd3e8132d7e5a216c93c938f67d46b8
Tyler Thomas <tygthomas0@gmail.com>
2021-06-15 16:39:00 -0600

    added comments

--- level-1/fizzbuzz/main.js
+++ level-1/fizzbuzz/main.js
@@ -1,17 +1,19 @@
+//counters for each phrase
 var fizzCount = 0;
 var buzzCount = 0;
 var fizzbuzzCount = 0;
 
+//loops over numbers 1-100
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
+    if (i % 3 == 0 && i % 5 == 0) { //if number is divisible by both 3 and 5, it is replaced with "fizzbuzz"
         console.log("fizzbuzz");
         fizzbuzzCount++;
     }
-    else if (i % 3 == 0) {
+    else if (i % 3 == 0) { //if number is divisible by only 3, it is replaced with "fizz"
         console.log("fizz");
         fizzCount++;
     }
-    else if (i % 5 == 0) {
+    else if (i % 5 == 0) { //if number is divisible by only 5, it is replaced with "buzz"
         console.log("buzz");
         buzzCount++;
     }
@@ -20,6 +22,7 @@ for (let i = 1; i <= 100; i++) {
     }
 }
 
+//creates object that keeps track of how many times each phrase was present
 function Counter(fizz, buzz, fizzbuzz) {
     this.fizz = fizz,
     this.buzz = buzz,
