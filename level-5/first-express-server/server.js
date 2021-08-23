e42c0be1b3276219cf3b747f673579c48a283a59
Tyler Thomas <tygthomas0@gmail.com>
2021-08-23 09:35:05 -0600

    added routes

--- level-5/first-express-server/server.js
+++ level-5/first-express-server/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const uuid = require("uuid/v4")
+const { v4 } = require("uuid")
 
 
 /*
@@ -13,24 +13,12 @@ app.get("/", (req, res) => {
 app.use(/* "/", */ express.json()) //looks for a request body, and turns it into "req.body"
 //first argument is optional, if you don't put a pathway at the beginning, it will run the function every time
 
-//can also set up "your own data":
 
-const movies = [
-    { title: "die hard", genre: "action", _id: uuid() },
-    { title: "star wars", genre: "fantasy", _id: uuid() },
-    { title: "lion king", genre: "fantasy", _id: uuid() },
-    { title: "friday the 13th", genre: "horror", _id: uuid() }
-]
+//routes
+app.use("/movies", require("./routes/movieRouter.js"))
+app.use("/tvshows", require("./routes/tvshowRouter.js"))
 
-app.get("/movies", (req, res) => {
-    res.send(movies)
-})
 
-app.post("/movies", (req, res) => {
-    req.body._id = uuid()
-    movies.push(req.body)
-    res.send(`Successfully added ${req.body.title} to database!`)
-})
 
 
 app.listen(9000, () => {
